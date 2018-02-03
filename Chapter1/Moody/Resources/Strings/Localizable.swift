bd5b309c54dd3b6324e29887f6bed5cf9f948e32
raditz2488 <rohanbhale@gmail.com>
2018-02-03 20:05:30 +0530

    1. Copied CameraViewController from original tutorial. 2. Copied Localizable from original tutorial. 3. Copied CameraView from original tutorial. 4. Copied CaptureSession from original tutorial

---
+++ Chapter1/Moody/Resources/Strings/Localizable.swift
@@ -0,0 +1,27 @@
+//
+//  Localizable.swift
+//  Moody
+//
+//  Created by pranav on 03/02/18.
+//  Copyright © 2018 RB. All rights reserved.
+//
+
+import Foundation
+
+enum LocalizedText: String {
+    case cameraView_tapToCapture = "CameraView.tapToCapture"
+    case cameraView_needAccess = "CameraView.needAccess"
+    case mood_dateComponentFormat = "Mood.dateComponentFormat"
+}
+
+func localized(_ key: LocalizedText) -> String {
+    return NSLocalizedString(key.rawValue, tableName: nil, bundle: Bundle.main, value: key.rawValue, comment: "")
+}
+
+
+func localized(_ key: LocalizedText, args: [CVarArg]) -> String {
+    let format = localized(key)
+    return withVaList(args) { arguments -> String in
+        return NSString(format: format, locale: NSLocale.current, arguments: arguments) as String
+    }
+}
