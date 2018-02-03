fde41566ee4fcb2fb09211cba89414c7ddcc2938
raditz2488 <rohanbhale@gmail.com>
2018-02-03 14:16:55 +0530

    1. Created RootViewController class. 2. Create a property in RootViewController class to hold managedObjectcontext. 3. Added code to load RootViewController from storyboard and also assign it the managegObjectContext. It is also added as rootViewController of the window here.

---
+++ Chapter1/Moody/Controllers/RootViewController.swift
@@ -0,0 +1,14 @@
+//
+//  RootViewController.swift
+//  Moody
+//
+//  Created by pranav on 03/02/18.
+//  Copyright © 2018 RB. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class RootViewController: UIViewController {
+    var managedObjectContext: NSManagedObjectContext!
+}
